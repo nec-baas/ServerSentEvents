aae3ccbc092ae72500a44b6b330524bc2e09c02b
Kwang Yul Seo <kwangyul.seo@gmail.com>
2015-04-26 11:07:24 +0900

    Add ServerSentEvents.Tests project.

---
+++ ServerSentEvents.Tests/Tests.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Kwang Yul Seo. All rights reserved.
+// Licensed under the Apache 2.0 license. See LICENSE file in the project root for full license information.
+
+using System;
+
+namespace ServerSentEvents.Tests
+{
+    public class Tests
+    {
+        public Tests()
+        {
+        }
+    }
+}
+
