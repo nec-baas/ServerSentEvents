48b614417eb2aa718cf22d16c90703d45646ba35
Kwang Yul Seo <kwangyul.seo@gmail.com>
2015-04-26 20:39:25 +0900

    Set up the test web server.

--- ServerSentEvents.Tests/Tests.cs
+++ ServerSentEvents.Tests/Tests.cs
@@ -1,12 +1,29 @@
 ﻿// Copyright (c) Kwang Yul Seo. All rights reserved.
 // Licensed under the Apache 2.0 license. See LICENSE file in the project root for full license information.
 
+using NUnit.Framework;
 using System;
 
 namespace ServerSentEvents.Tests
 {
+    [TestFixture]
     public class Tests
     {
+        private TestWebServer ws;
+
+        [TestFixtureSetUp]
+        public void ServerSetUp()
+        {
+            ws = new TestWebServer(new Uri("http://localhost:8080"));
+            ws.Start();
+        }
+
+        [TestFixtureTearDown]
+        public void ServerTearDown()
+        {
+            ws.Stop();
+        }
+
         public Tests()
         {
         }
