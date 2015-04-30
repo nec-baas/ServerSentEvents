0b04ae5a4b87b6b12a6198b27c1ab01efff2c2c6
Kwang Yul Seo <kwangyul.seo@gmail.com>
2015-04-30 17:03:02 +0900

    Remove unused imports.

--- ServerSentEvents/EventSource.cs
+++ ServerSentEvents/EventSource.cs
@@ -6,8 +6,6 @@ using System.Reactive;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace ServerSentEvents
 {
